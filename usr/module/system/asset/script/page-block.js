1fe23e17b78500c1ad283265ce72497f0ae57cab
sexnothing <sexnothing@gmail.com>
2013-07-11 15:43:53 +0800

    Use grunt jsbeautifier and jshint handler js files, all js files will be checked by this tow tools, before submit it, all js files will be not throw error

--- usr/module/system/asset/script/page-block.js
+++ usr/module/system/asset/script/page-block.js
@@ -1,23 +1,23 @@
-(function ($) {
+(function($) {
   var ModuleListItemView = Backbone.View.extend({
     template: _.template($("#module-temp").html()),
     templateBlock: _.template($("#module-block-temp").html()),
     events: {
       "click .module-header": "loadBlock"
     },
-    initialize: function () {},
-    render: function () {
+    initialize: function() {},
+    render: function() {
       this.$el.html(this.template(this.model.toJSON()));
       return this;
     },
-    loadBlock: function () {
+    loadBlock: function() {
       var h = this.$(".module-header");
       var b = this.$(".module-blocks");
       var tb = this.templateBlock;
       var self = this;
       h.toggleClass("active");
       if (!h.attr("data-loaded")) {
-        $.getJSON('/admin/system/page/blocklist/name/' + this.model.get("name")).done(function (res) {
+        $.getJSON('/admin/system/page/blocklist/name/' + this.model.get("name")).done(function(res) {
           var data = res.data;
           if (data.length) {
             for (var i = 0, l = data.length; i < l; i++) {
@@ -34,12 +34,12 @@
         b.slideToggle("300");
       }
     },
-    dragBlock: function (el) {
+    dragBlock: function(el) {
       el.draggable({
         helper: "clone",
         handle: ".module-block-title"
       });
-      el.each(function () {
+      el.each(function() {
         var $this = $(this);
         if ($("[id*=pi-zone-]").find(".widget-clone[data-id=" + $this.attr("data-id") + "]").length) {
           $this.draggable("option", "disabled", true);
@@ -49,24 +49,24 @@
   });
   var ModuleListView = Backbone.View.extend({
     el: $("#widget-wrap"),
-    initialize: function () {
+    initialize: function() {
       this.render();
     },
-    render: function () {
+    render: function() {
       this.collection.forEach(this.addOne, this);
     },
     addOne: function(model) {
       this.$el.append(new ModuleListItemView({
-          model: model
+        model: model
       }).render().el);
     }
   });
   var PageItemView = Backbone.View.extend({
     template: _.template($("#page-area-temp").html()),
-    initialize: function () {
+    initialize: function() {
       this.render();
     },
-    render: function () {
+    render: function() {
       var m = this.model;
       var el = this.$el;
       for (var i = 0, l = m.length; i < l; i++) {
@@ -82,18 +82,18 @@
       "click .load-theme": "loadTheme",
       "click .set-theme-item": "setTheme"
     },
-    initialize: function (options) {
+    initialize: function(options) {
       this.page = options.page;
       $('.submit-theme').click(_.bind(this.submitTheme, this));
     },
-    loadTheme: function () {
+    loadTheme: function() {
       var w = this.$(".theme-wrap");
       var self = this;
       var u = w.find("ul");
       w.fadeToggle("200");
       this.$('.load-theme').toggleClass('active');
       if (!w.attr("data-load")) {
-        $.getJSON('/admin/system/page/themelist/').done(function (result) {
+        $.getJSON('/admin/system/page/themelist/').done(function(result) {
           u.html(_.template(self.template, _.toArray(result), {
             variable: 'data'
           }));
@@ -101,7 +101,7 @@
         });
       }
     },
-    submitTheme: function () {
+    submitTheme: function() {
       var data = {
         page: this.page,
         blocks: {}
@@ -110,16 +110,16 @@
       var ret;
       for (var i = 0; i < 100; i++) {
         ret = [];
-        b.filter(function(index, el){
-          return ~$(el).attr('id').indexOf('-' + i + '-');
-        }).find(".widget-clone").each(function () {
-          ret.push(parseInt($(this).attr("data-id")));
+        b.filter(function(index, el) {
+          return~ $(el).attr('id').indexOf('-' + i + '-');
+        }).find(".widget-clone").each(function() {
+          ret.push(parseInt($(this).attr("data-id"), 10));
         });
         if (ret.length) {
           data.blocks[i] = ret;
         }
       }
-      $.post('/admin/system/page/save', data).done(function (res) {
+      $.post('/admin/system/page/save', data).done(function(res) {
         res = $.parseJSON(res);
         if (res.status) {
           systemMessage.succ(res.message);
@@ -128,29 +128,30 @@
         }
       });
     },
-    setTheme: function (e) {
+    setTheme: function(e) {
       this.$(".set-theme-item").removeClass("active");
       var tar = $(e.currentTarget);
       tar.addClass("active");
-      $.get('/admin/system/page/zonetemplate/theme/' + tar.attr("data-name")).done(function (res) {
+      $.get('/admin/system/page/zonetemplate/theme/' + tar.attr("data-name")).done(function(res) {
         var org = [];
-        for (var i = 0; i < 100; i++) {
+        var i;
+        for (i = 0; i < 100; i++) {
           org.push($("#pi-zone-" + i + "-edit").find(".ui-placeholder,.pi-zone-num").remove().end().html());
         }
         $("#drag-main").html(res);
-        for (var i = 0; i < 100; i++) {
+        for (i = 0; i < 100; i++) {
           $("#pi-zone-" + i + "-edit").html(org[i]);
         }
         initPageEvent();
       });
     }
   });
-  var initPageEvent = function () {
+  var initPageEvent = function() {
     var drag = $("[id*=pi-zone-]");
     drag.droppable({
       accept: "#widget-wrap .module-block",
       hoverClass: "ui-state-hover",
-      drop: function (event, ui) {
+      drop: function(event, ui) {
         var h = ui.draggable,
           m = h.data("info");
         h.draggable("option", "disabled", true);
@@ -161,7 +162,7 @@
         })).insertBefore($(this).find(".ui-placeholder"));
       }
     }).append("<div class='ui-placeholder'></div>");
-    drag.each(function () {
+    drag.each(function() {
       var $this = $(this);
       $this.css("min-height", $this.parent().height());
       $this.prepend("<div class='pi-zone-num'>" + $this.attr("id").replace(/[a-z-]/g, "") + "</div>");
@@ -172,16 +173,16 @@
       items: ".widget-clone",
       revert: 300
     });
-    drag.on("click", ".close", function (e) {
+    drag.on("click", ".close", function(e) {
       var tar = $(e.target),
         p = tar.parents(".widget-clone");
-      p.fadeOut(100, function () {
+      p.fadeOut(100, function() {
         p.remove();
         $("#" + p.attr("id").replace(/\-clone$/, "")).draggable("option", "disabled", false);
       });
     });
-  }
-  this.pageBlcokAction = function (options) {
+  };
+  this.pageBlcokAction = function(options) {
     new ModuleListView({
       collection: new Backbone.Collection(options.modules)
     });
@@ -189,7 +190,7 @@
     for (var i in blocks) {
       if (blocks.hasOwnProperty(i)) {
         new PageItemView({
-          el: $("#pi-zone-" + parseInt(i) + "-edit"),
+          el: $("#pi-zone-" + parseInt(i, 10) + "-edit"),
           model: blocks[i]
         });
       }
@@ -199,4 +200,4 @@
     });
     initPageEvent();
   };
-})(jQuery)
+})(jQuery);
