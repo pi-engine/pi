6832b1a5fc6dc30a3fa448f62a3f2dc6ed4cb3ea
Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
2013-07-26 00:45:35 +0800

    docblock fix in process

--- usr/editor/ckeditor/src/Renderer.php
+++ usr/editor/ckeditor/src/Renderer.php
@@ -1,32 +1,25 @@
 <?php
 /**
- * Pi Engine Ckeditor Editor Renderer
+ * Pi Engine (http://pialog.org)
  *
- * You may not change or alter any portion of this comment or credits
- * of supporting developers from this source code or any supporting source code
- * which is considered copyrighted (c) material of the original comment or credit authors.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * @copyright       Copyright (c) Pi Engine http://www.xoopsengine.org
- * @license         http://www.xoopsengine.org/license New BSD License
- * @author          Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
- * @since           3.0
- * @package         Editor\Ckeditor
- * @version         $Id$
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
  */
-
 
 namespace Editor\Ckeditor;
 
 use Pi;
 use Pi\Editor\AbstractRenderer;
 use Zend\Form\ElementInterface;
-//use Zend\View\Renderer\RendererInterface as Renderer;
 use CKFinder;
 use Ckeditor;
 
+/**
+ * CKeditor renderer
+ *
+ * @author Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
+ */
 class Renderer extends AbstractRenderer
 {
     protected $configFile = 'editor.ckeditor.php';
--- usr/editor/markitup/src/Renderer.php
+++ usr/editor/markitup/src/Renderer.php
@@ -1,29 +1,23 @@
 <?php
 /**
- * Pi Engine Markitup Editor Renderer
+ * Pi Engine (http://pialog.org)
  *
- * You may not change or alter any portion of this comment or credits
- * of supporting developers from this source code or any supporting source code
- * which is considered copyrighted (c) material of the original comment or credit authors.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * @copyright       Copyright (c) Pi Engine http://www.xoopsengine.org
- * @license         http://www.xoopsengine.org/license New BSD License
- * @author          Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
- * @since           3.0
- * @package         Editor\Markitup
- * @version         $Id$
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
  */
 
 namespace Editor\Markitup;
 
 use Pi;
 use Pi\Editor\AbstractRenderer;
-//use Zend\View\Renderer\RendererInterface as Renderer;
 use Zend\Form\ElementInterface;
 
+/**
+ * Markitup renderer
+ *
+ * @author Taiwen Jiang <taiwenjiang@tsinghua.org.cn> 
+ */
 class Renderer extends AbstractRenderer
 {
     protected $configFile = 'editor.markitup.php';
