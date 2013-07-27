68f40172be91b979a82b260bc3759533213bdcbb
Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
2013-07-27 11:04:20 +0800

    Restored missing files

---
+++ lib/Pi/Mvc/Router/Http/User.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
+ */
+
+namespace Pi\Mvc\Router\Http;
+
+use Module\System\Route\User as SystemUser;
+
+/**
+ * System user route
+ *
+ * @deprecated
+ * @see \Module\System\Route\User
+ * @author Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
+ */
+class User extends SystemUser
+{}
