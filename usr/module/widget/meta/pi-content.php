a10fb5468f58ba73dc4e3de4faff118d0214e5a9
Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
2013-08-06 13:39:46 +0800

    CS fix in process

--- usr/module/widget/meta/pi-content.php
+++ usr/module/widget/meta/pi-content.php
@@ -1,21 +1,12 @@
 <?php
 /**
- * Widget for fetching Pi Engine content
+ * Pi Engine (http://pialog.org)
  *
- * You may not change or alter any portion of this comment or credits
- * of supporting developers from this source code or any supporting source code
- * which is considered copyrighted (c) material of the original comment or credit authors.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * @copyright       Copyright (c) Pi Engine http://www.xoopsengine.org
- * @license         http://www.xoopsengine.org/license New BSD License
- * @author          Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
- * @package         Module\Widget
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
  */
 
-// Widget meta
 return array(
     'title'         => __('Pi Engine top contents'),
     'description'   => __('Block to display Pi Engine top and hot contents'),
