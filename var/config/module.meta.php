e4cf40b272143bf48293bde3bead7aab06a87db9
Lin Zongshu <lin40553024@163.com>
2013-09-03 17:54:02 +0800

    Completed avatar configs

---
+++ var/config/module.meta.php
@@ -0,0 +1,23 @@
+<?php
+return array (
+  'system' => 
+  array (
+    'directory' => 'system',
+    'active' => '1',
+  ),
+  'page' => 
+  array (
+    'directory' => 'page',
+    'active' => '1',
+  ),
+  'widget' => 
+  array (
+    'directory' => 'widget',
+    'active' => '1',
+  ),
+  'user' => 
+  array (
+    'directory' => 'user',
+    'active' => '1',
+  ),
+);
