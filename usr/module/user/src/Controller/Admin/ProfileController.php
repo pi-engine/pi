20ee669c8670e0614d345de7fe675f80942e40ca
liuchuangww <liuchuang@eefocus.com>
2013-09-09 19:55:47 +0800

    Add profile display file managee

--- usr/module/user/src/Controller/Admin/ProfileController.php
+++ usr/module/user/src/Controller/Admin/ProfileController.php
@@ -61,8 +61,10 @@ class ProfileController extends ActionController
         }
 
 
-        vd($profile);
-        vd($compounds);
+
+
+        //vd($profile);
+        //vd($compounds);
 
         $this->view()->assign(array(
             'profile'   => $profile,
@@ -74,6 +76,50 @@ class ProfileController extends ActionController
     public function privacyAction()
     {
 
+    }
+
+    public function testAction()
+    {
+        $this->view()->setTemplate(false);
+        vd($this->getGroupDisplay());
+    }
+
+    protected function getGroupDisplay()
+    {
+        $result = array();
+        $groupModel = $this->getModel('display_group');
+        $select = $groupModel->select(array());
+        $rowset = $groupModel->selectWith($select);
+
+        foreach ($rowset as $row) {
+            $result[$row['name']] = array(
+                'name'     => $row['name'],
+                'title'    => $row['title'],
+                'compound' => $row['compound'],
+            );
+
+            $displayFieldModel = $this->getModel('display_filed');
+            $select = $displayFieldModel->select(array('group' => $row['name']));
+            $displayFieldRowset = $displayFieldModel->selectWith($select);
+
+            $fields = array();
+            foreach ($displayFieldRowset as $field) {
+                $fields[$field['field']] = array();
+            }
+            if ($row['compound']) {
+                $compoundMeta = Pi::registry('compound', 'user')->read(
+                    $row['compound']
+                );
+
+                foreach ($compoundMeta as $name => $meta) {
+                    if (isset($fields[$name])) {
+                        $fields[$name] = array(
+                            'name'
+                        );
+                    }
+                }
+            }
+        }
     }
 
 }
