80ffda274df78c9a4df19a2226075134ec8373d0
liuchuangww <liuchuang@eefocus.com>
2013-09-12 23:55:59 +0800

    Complete advanced search and add from preview

---
+++ usr/module/user/src/Controller/Admin/FormController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
+ */
+
+namespace Module\User\Controller\Admin;
+
+use Pi;
+use Pi\Mvc\Controller\ActionController;
+
+/**
+ * Form preview controller
+ *
+ * @author Liu Chuang <liuchuang@eefocus.com>
+ */
+class FormController extends ActionController
+{
+
+}
