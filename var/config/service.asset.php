c342832d8cc5322df87567d0ede9db51a94c9976
Taiwen <taiwenjiang@tsinghua.org.cn>
2013-09-30 10:21:43 +0800

    Added missing service config file

---
+++ var/config/service.asset.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Asset service configuration
+ */
+
+return array(
+    // Append version number to asset files to prevent browser cache
+    'append_version'        => true,
+
+    // Use symlink for asset publish
+    'use_symlink'           => true,
+);
