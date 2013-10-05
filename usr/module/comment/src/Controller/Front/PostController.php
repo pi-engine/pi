156dd5d090589d8f7e046065a354c4fc2233dfdf
Taiwen <taiwenjiang@tsinghua.org.cn>
2013-10-05 17:14:18 +0800

    Comment post operations on post view page

--- usr/module/comment/src/Controller/Front/PostController.php
+++ usr/module/comment/src/Controller/Front/PostController.php
@@ -38,6 +38,28 @@ class PostController extends ActionController
             $user['url'] =  Pi::service('user')->getUrl('profile', $post['uid']);
             $user['avatar'] = Pi::service('avatar')->get($post['uid']);
             $post['user'] = $user;
+            $active = $post['active'];
+            $post['operations'] = array(
+                'edit'  => array(
+                    'title' => __('Edit'),
+                    'url'   => Pi::api('comment')->getUrl('edit', array(
+                        'post'  => $id,
+                    )),
+                ),
+                'delete'  => array(
+                    'title' => __('Delete'),
+                    'url'   => Pi::api('comment')->getUrl('delete', array(
+                        'post'  => $id,
+                    )),
+                ),
+                'approve'  => array(
+                    'title' => $active ? __('Disable') : __('Enable'),
+                    'url'   => Pi::api('comment')->getUrl('approve', array(
+                        'post'  => $id,
+                        'flag'  => $active ? 0 : 1,
+                    )),
+                ),
+            );
         }
         $title = __('Comment post');
         $this->view()->assign('comment', array(
