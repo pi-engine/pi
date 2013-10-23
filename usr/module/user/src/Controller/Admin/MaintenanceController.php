74b7da9948748bb1df50db36637e319a8030a5fb
liuchuangww <liuchuang@eefocus.com>
2013-10-23 18:00:15 +0800

    Fix user empty log

--- usr/module/user/src/Controller/Admin/MaintenanceController.php
+++ usr/module/user/src/Controller/Admin/MaintenanceController.php
@@ -115,7 +115,7 @@ class MaintenanceController extends ActionController
             'users'      => $logs,
             'paginator' => $paginator,
         );
-
+d($data);exit;
         return $data;
 
     }
@@ -427,7 +427,12 @@ class MaintenanceController extends ActionController
             if (isset($userData[$uid]['login_times'])) {
                 $data['login_times'] = $userData[$uid]['login_times']['value_int'];
             }
-            $logs[] = array_merge($profile, $data);
+            if ($data) {
+                $logs[] = array_merge($profile, $data);
+            } else {
+                $logs[] = $profile;
+            }
+
             unset($profile);
             unset($data);
         }
