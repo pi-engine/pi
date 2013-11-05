792a4754577724857407f0c111b08f2a51c6c752
Taiwen <taiwenjiang@tsinghua.org.cn>
2013-11-05 19:13:48 +0800

    Added duplication protection against js/css

--- lib/Pi/View/Helper/Css.php
+++ lib/Pi/View/Helper/Css.php
@@ -60,6 +60,27 @@ class Css extends AssetCanonize
      */
     public function __invoke($files = null, $attributes = 'append')
     {
+        if (null === $files) {
+            return $this;
+        }
+
+        $this->load($files, $attributes);
+
+        return $this;
+    }
+
+    /**
+     * Load CSS files
+     *
+     * @param   string|array $files
+     * @param   string|array $attributes
+     *      Only applicable when $files is scalar,
+     *      default as string for position,
+     *      append or prepend, default as 'append'
+     * @return  self
+     */
+    public function load($files = null, $attributes = 'append')
+    {
         $files = $this->canonize($files, $attributes);
         $helper = $this->view->headLink();
         foreach ($files as $file => $attrs) {
--- lib/Pi/View/Helper/Js.php
+++ lib/Pi/View/Helper/Js.php
@@ -60,6 +60,27 @@ class Js extends AssetCanonize
      */
     public function __invoke($files = null, $attributes = 'append')
     {
+        if (null === $files) {
+            return $this;
+        }
+
+        $this->load($files, $attributes);
+
+        return $this;
+    }
+
+    /**
+     * Load JavaScript file
+     *
+     * @param   string|array $files
+     * @param   string|array $attributes
+     *      Only applicable when $files is scalar,
+     *      default as string for position,
+     *      append or prepend, default as 'append'
+     * @return  self
+     */
+    public function load($files = null, $attributes = 'append')
+    {
         $files = $this->canonize($files, $attributes);
         $helper = $this->view->headScript();
         foreach ($files as $file => $attrs) {
