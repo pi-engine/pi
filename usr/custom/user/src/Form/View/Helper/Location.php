4d034019e0d4dae3a92e8c34ea7128026bcd7112
sexnothing@gmail.com <sexnothing@gmail.com>
2013-11-06 10:34:07 +0800

    EEFOCUS custom field

--- usr/custom/user/src/Form/View/Helper/Location.php
+++ usr/custom/user/src/Form/View/Helper/Location.php
@@ -48,15 +48,18 @@ class Location extends AbstractHelper
      */
     public function render(ElementInterface $element)
     {
-        $html = <<<EOD
-        <script src="%s"></script>
-        <div id="js-country-element" data-value="%s">
+        Pi::service('view')->getHelper('js')->load(Pi::url('static/js/eefocus-linkage.js'));
+        $id = uniqid();
+
+        return sprintf('
+        <div id="%s" data-value="%s">
         </div>
         <script>
-            new EEFOCUS_DATA.Linkage("js-country-element", ["country", "province", "city"]);
-        </script>
-EOD;
-
-        return sprintf($html, Pi::url('static/js/eefocus-data.js'), $element->getValue());
+            new eefocus.Linkage("%s", ["country", "province", "city"]);
+        </script> 
+        ',
+        $id,
+        $element->getValue(),
+        $id);
     }
 }
