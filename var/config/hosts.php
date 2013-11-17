93f83e72272df4abbbb3315571ea8c7484dd5bc4
Taiwen <taiwenjiang@tsinghua.org.cn>
2013-11-17 14:35:36 +0800

    Updated custom path default setting

--- var/config/hosts.php
+++ var/config/hosts.php
@@ -42,7 +42,7 @@ return array(
                 'var'       => '/path/to/pi/var',
                 'module'    => '/path/to/pi/usr/module',
                 'theme'     => '/path/to/pi/usr/theme',
-                'custom'    => '/path/to/pi/usr/module/custom',
+                'custom'    => '/path/to/pi/usr/custom',
 
                 'upload'    => '/path/to/pi/upload',
                 'asset'     => '/path/to/pi/asset',
