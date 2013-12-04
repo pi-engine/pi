56dac73f009d8df5aef1b3be508e40f3838089e3
sexnothing@gmail.com <sexnothing@gmail.com>
2013-12-04 15:54:53 +0800

    Layout admin update

---
+++ usr/module/system/asset/admin/dashboard.js
@@ -0,0 +1,79 @@
+﻿angular.module('systemDashboardModule')
+.config(['$routeProvider', 'piProvider', 'config',
+  function($routeProvider, piProvider, config) {
+     function tpl(name) {
+      return config.assetRoot + name + '.html';
+    }
+
+    $routeProvider.otherwise({
+      templateUrl: tpl('dashboard'),
+      controller: 'DashboardCtrl'
+    });
+    piProvider.translations(config.t);
+    piProvider.ajaxSetup();
+  }
+])
+.directive('noticeFocus', ['$timeout',
+  function($timeout) {
+    return function(scope, elem, attrs) {
+      scope.$watch(attrs.noticeFocus, function (newVal) {
+        if (newVal) {
+          $timeout(function () {
+            elem[0].focus();
+          }, 0, false);
+        }
+      });
+    }
+  }
+])
+.service('server', ['$http', 'config',
+  function($http, config) {
+    var urlRoot = config.urlRoot;
+
+    this.data = config.data;
+
+    this.saveNotice = function(content) {
+      return $http.post(urlRoot + 'message', {
+        content: content
+      });
+    }
+
+    this.saveLinks = function(links) {
+      return $http.post(urlRoot + 'link', {
+        content: links
+      });
+    }
+  }
+])
+.controller('DashboardCtrl', ['$scope', 'server',
+  function($scope, server) {
+    angular.extend($scope, server.data);
+
+    $scope.messageContentCopy = $scope.message.content;
+
+    $scope.saveNoticeAction = function() {
+      server.saveNotice($scope.messageContentCopy).success(function(data) {
+        $scope.message = data;
+        $scope.noticeEditing = 0;
+        $scope.messageContentCopy = data.message.content;
+      });
+    }
+
+    $scope.removeLink = function(idx) {
+      $scope.links.splice(idx, 1);
+    }
+
+    $scope.addLink = function() {
+      var item = angular.copy($scope.linkEntity);
+      $scope.links.push(item);
+      $scope.linkEntity = {};
+    }
+
+    $scope.saveLinksAction = function() {
+      server.saveLinks($scope.links).success(function(data) {
+        $scope.linksEditing = 0;
+        $scope.linkAddStatus = 0;
+      });
+    }
+  }
+]);
