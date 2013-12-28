6f53d7c84c21cabdc065a31bdfc0db168387b173
Taiwen <taiwenjiang@tsinghua.org.cn>
2013-12-28 16:26:35 +0800

    backup

---
+++ lib/Pi/Application/Installer/Schema/AbstractUpdator.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
+ */
+
+namespace   Pi\Application\Installer\Schema;
+
+use Pi;
+use Pi\Application\Installer\Action\Update as UpdateAction;
+use Pi\Application\Installer\SqlSchema;
+
+/**
+ * Module schema update handler
+ *
+ * @author Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
+ */
+class AbstractUpdator
+{
+    /** @var UpdateAction */
+    protected $handler;
+
+    /**
+     * Constructor
+     *
+     * @param UpdateAction $handler
+     */
+    public function __construct(UpdateAction $handler)
+    {
+        $this->handler = $handler;
+    }
+
+    /**
+     * Alter system table schema
+     *
+     * @param string $sql
+     *
+     * @return bool
+     */
+    protected function queryTable($sql)
+    {
+        $adapter = Pi::db()->getAdapter();
+        try {
+            $adapter->query($sql, 'execute');
+        } catch (\Exception $exception) {
+            $this->handler->setResult('db', array(
+                'status'    => false,
+                'message'   => 'Table alter query failed: '
+                    . $exception->getMessage(),
+            ));
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Update system table schema
+     *
+     * @param string $sql
+     *
+     * @return bool
+     */
+    protected function querySchema($sql)
+    {
+        $sqlHandler = new SqlSchema;
+        try {
+            $sqlHandler->queryContent($sql);
+        } catch (\Exception $exception) {
+            $this->handler->setResult('db', array(
+                'status'    => false,
+                'message'   => 'SQL schema query failed: '
+                    . $exception->getMessage(),
+            ));
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Update module table schema
+     *
+     * @param string       $version
+     *
+     * @throws \Exception
+     * @return bool
+     */
+    public function upgrade($version)
+    {
+        throw new \Exception('Method not implemented yet.');
+    }
+}
---
+++ usr/module/comment/src/Model/Type.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
+ */
+
+namespace Module\Comment\Model;
+
+use Pi\Application\Model\Model;
+
+/**
+ * Comment type model
+ *
+ * @author Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
+ */
+class Type extends Model
+{
+    /**
+     * Columns to be encoded
+     *
+     * @var array
+     */
+    protected $encodeColumns = array(
+        'params'  => true,
+    );
+}
