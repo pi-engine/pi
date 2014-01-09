5ec28bb7b897b194638693c1e5b715ba3d08868d
unknown <sexnothing@gmail.com>
2014-01-08 17:20:51 -0800

    Default theme navbar fixed

--- usr/theme/default/asset/js/front.js
+++ usr/theme/default/asset/js/front.js
@@ -1,6 +1,7 @@
 (function($) {
   //Fix navbar multiple level
   var navbar = $('.pi-navbar-nav');
+  var hasBrand = navbar.parents('.navbar').find('.navbar-brand').length;
   navbar.find('>li').each(function() {
     var $this = $(this);
     var caretStr = '<span class="pi-navbar-caret"></span>';
@@ -10,4 +11,8 @@
     }
   });
   navbar.find('ul').addClass('dropdown-menu');
+
+  if (!hasBrand) {
+    navbar.css('marginLeft', '-15px');
+  }
 })(jQuery)
