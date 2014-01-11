b2811849a1c09c7d42709eee9e0ef8dc09b1c81e
unknown <sexnothing@gmail.com>
2014-01-10 18:16:27 -0800

    Sidenav.js update

--- www/static/vendor/jquery/pi/sidenav/sidenav.js
+++ www/static/vendor/jquery/pi/sidenav/sidenav.js
@@ -1,6 +1,4 @@
 /**
- * <link href="<?php echo Pi::url('static/component/sidenav/pi-sidenav.css'); ?>" rel="stylesheet">
- * <script src="<?php echo Pi::url('static/component/sidenav/pi-sidenav.js'); ?>"></script>
  * Example
  * <ul class="pi-sidenav">
     <li class="active">
