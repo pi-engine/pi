bf930a4663bba8b0def17b63d3d0cf5f4e686038
Taiwen <taiwenjiang@tsinghua.org.cn>
2014-01-23 11:28:46 +0800

    Added module custom breadcrumbs support

---
+++ usr/module/demo/src/Api/Breadcrumbs.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
+ */
+
+namespace Module\Demo\Api;
+
+use Pi;
+use Pi\Application\AbstractBreadcrumbs;
+
+class Breadcrumbs extends AbstractBreadcrumbs
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $module = 'demo';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load()
+    {
+        $result = array(
+            array(
+                'label' => __('Demo breadcrumbs'),
+                'href'  => Pi::url('www'),
+            )
+        );
+
+        return $result;
+    }
+}
