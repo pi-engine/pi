4287744a60c77cb44809bbf824a9b5db16f2aaf3
lavenderli <lisibei1212@gmail.com>
2014-01-26 19:47:56 +0800

    Fix for search content

--- usr/module/article/src/Api/Search.php
+++ usr/module/article/src/Api/Search.php
@@ -44,6 +44,15 @@ class Search extends AbstractSearch
         'time_publish'  => 'time',
         'uid'           => 'uid',
     );
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected function buildContent($content = '')
+    {
+        $content = mb_substr(strip_tags($content), 0, 255, 'utf-8');
+        return $content;
+    }
 
     /**
      * {@inheritDoc}
