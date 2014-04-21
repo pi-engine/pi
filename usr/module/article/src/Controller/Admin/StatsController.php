4eb594e778b9bc52520c3095132d972028c32692
Taiwen <taiwenjiang@tsinghua.org.cn>
2014-04-21 19:13:26 +0800

    Fix dependence on tag module

--- usr/module/article/src/Controller/Admin/StatsController.php
+++ usr/module/article/src/Controller/Admin/StatsController.php
@@ -42,8 +42,8 @@ class StatsController extends ActionController
         $topSubmitters7    = Stats::getSubmittersRecently(7, 10);
         $topSubmitters30   = Stats::getSubmittersRecently(30, 10);
 
-        if ($this->config('enable_tag')) {
-            $topTags = Pi::api('api', 'tag')->top(10, $this->getModule(), null);
+        if ($this->config('enable_tag') && Pi::service('tag')->active()) {
+            $topTags = Pi::service('tag')->top(10, $this->getModule(), null);
             $this->view()->assign('topTags', $topTags);
         }
 
