220d4eebfb8e4ec464cf10bbfb2c2e9072685350
Taiwen <taiwenjiang@tsinghua.org.cn>
2014-05-12 18:39:29 +0800

    Added stylesheet for preview

--- usr/module/widget/src/Controller/Front/IndexController.php
+++ usr/module/widget/src/Controller/Front/IndexController.php
@@ -9,10 +9,16 @@
 
 namespace Module\Widget\Controller\Front;
 
+use Pi;
 use Pi\Mvc\Controller\ActionController;
 
 /**
- * Placeholder controller
+ * Block preview controller
+ *
+ * Usage
+ * ```
+ *  http://pi.tld/widget/?block=1,2,3&zone=2,6,9&theme=pi
+ * ```
  *
  * @author Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
  */
@@ -20,17 +26,61 @@ class IndexController extends ActionController
 {
     public function indexAction()
     {
-        $id = (int) _get('block');
-        $list = array();
+        $block  = _get('block');
+        $zone   = _get('zone');
+        $theme  = _get('theme');
+        $placeholder = array(array(
+            'title'         => __('Block title'),
+            'content'       => __('Block content placeholder.'),
+            'class'         => 'widget-preview-placeholder',
+
+            'subline'       => null,
+            'title_hidden'  => true,
+        ));
+
+        $parse = function ($input) {
+            if (!$input) {
+                return false;
+            }
+            $list = explode(',', $input);
+            array_walk($list, 'trim');
+            $list = array_filter($list);
+            array_walk($list, 'intval');
+            $list = array_unique($list);
+
+            return $list;
+        };
+        $block  = $parse($block);
+        $zone   = $parse($zone);
+
+        $list   = array();
         $blockLoader = $this->view()->helper('blocks');
-        if ($id) {
+        if ($block) {
             $blockRender = $this->view()->helper('block');
-            $block= $blockRender($id);
-            if ($block) {
-                $zones = $blockLoader->getZones();
-                foreach ($zones as $key => $zone) {
-                    $list[$key] = array($block);
+            $blocks = array();
+            foreach ($block as $id) {
+                $blockRow = $blockRender($id);
+                if ($blockRow) {
+                    $blocks[] = $blockRow;
                 }
+            }
+            if ($blocks) {
+                $zones = array_keys($blockLoader->getZones());
+                $zoneLoad = $zones;
+                $zoneEmpty = array();
+                if ($zone) {
+                    $zoneLoad = array_intersect($zones, $zone);
+                    $zoneEmpty = array_diff($zones, $zone);
+                }
+                foreach ($zoneLoad as $key) {
+                    $list[$key] = $blocks;
+                }
+                foreach ($zoneEmpty as $key) {
+                    $list[$key] = $placeholder;
+                }
+            }
+            if ($theme) {
+                Pi::service('theme')->setTheme($theme);
             }
         }
         $blockLoader->assign($list);
