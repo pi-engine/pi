d4589b7f5299b9344cfd1cc7c2a1b697ddffda8a
Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
2014-12-23 11:45:28 +0800

    Added alias of AbstractSearch

---
+++ lib/Pi/Application/Api/AbstractSearch.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt BSD 3-Clause License
+ */
+
+namespace Pi\Application\Api;
+
+use Pi\Search\AbstractSearch as SearchAbstract;
+
+/**
+ * Abstract class for Search API, alias of Pi\Search\AbstractSearch
+ *
+ * @author Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
+ */
+abstract class AbstractSearch extends SearchAbstract
+{}
