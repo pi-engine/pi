2116a97036609f3f234aa438ce52c2cde0db8798
voltan <djvoltan@gmail.com>
2015-12-31 09:34:27 +0330

    Update bootstrap-datepicker

--- www/public/vendor/bootstrap/datepicker/locales/bootstrap-datepicker.it.js
+++ www/public/vendor/bootstrap/datepicker/locales/bootstrap-datepicker.it.js
@@ -4,11 +4,15 @@
  */
 ;(function($){
 	$.fn.datepicker.dates['it'] = {
-		days: ["Domenica", "Lunedi", "Martedi", "Mercoledi", "Giovedi", "Venerdi", "Sabato", "Domenica"],
-		daysShort: ["Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom"],
-		daysMin: ["Do", "Lu", "Ma", "Me", "Gi", "Ve", "Sa", "Do"],
+		days: ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"],
+		daysShort: ["Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab"],
+		daysMin: ["Do", "Lu", "Ma", "Me", "Gi", "Ve", "Sa"],
 		months: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
 		monthsShort: ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"],
-		today: "Oggi"
+		today: "Oggi",
+		monthsTitle: "Mesi",
+		clear: "Cancella",
+		weekStart: 1,
+		format: "dd/mm/yyyy"
 	};
 }(jQuery));
