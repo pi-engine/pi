d333b409d406efa835efc8c2973d1c8b573fd236
Marc <marcd@nordnet.fr>
2016-01-05 20:26:22 +0100

    add back custom config.js

--- www/script/editor/ckeditor/config.js
+++ www/script/editor/ckeditor/config.js
@@ -4,7 +4,43 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+    // Define changes to default configuration here.
+    // For complete reference see:
+    // http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+    // The toolbar groups arrangement, optimized for two toolbar rows.
+    config.toolbarGroups = [
+        { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+        { name: 'links', groups: [ 'links' ] },
+        '/',
+        { name: 'insert', groups: [ 'insert' ] },
+        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+        { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+        { name: 'others', groups: [ 'others' ] },
+        '/',
+        { name: 'styles', groups: [ 'styles' ] },
+        { name: 'colors', groups: [ 'colors' ] },
+        { name: 'about', groups: [ 'about' ] },
+        { name: 'tools', groups: [ 'tools' ] }
+    ];
+
+    // Remove some buttons provided by the standard plugins, which are
+    // not needed in the Standard(s) toolbar.
+    config.removeButtons = 'Underline,Subscript,Superscript';
+
+    // Set the most common block elements.
+    config.format_tags = 'p;h1;h2;h3;pre';
+
+    // Simplify the dialog windows.
+    config.removeDialogTabs = 'image:advanced;link:advanced';
+
+    // Set editor height
+    config.height = 600;
+
+    // Set roxyFileman example config
+    // var roxyFileman = 'YOUR_PI_URL/script/editor/fileman/index.html?integration=ckeditor';
+    // config.filebrowserBrowseUrl = roxyFileman;
+    // config.filebrowserImageBrowseUrl = roxyFileman+'&type=image';
 };
