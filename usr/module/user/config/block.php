074d9cd3f7ea1363b643f388454c873275d86e0a
voltan <djvoltan@gmail.com>
2016-03-01 14:31:22 +0330

    Add Profile completeness block

---
+++ usr/module/user/config/block.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt BSD 3-Clause License
+ */
+
+/**
+ * User module meta
+ *
+ * @author Hossein Azizabadi <azizabadi@faragostaresh.com>
+ */
+return array(
+    'completeness' => array(
+        'title' => _a('Profile completeness'),
+        'description' => '',
+        'render' => array('block', 'completeness'),
+        'template' => 'completeness',
+        'config' => array(
+            'max_percent' => array(
+                'title' => _a('Max percent to active block visibility'),
+                'description' => '',
+                'edit' => 'text',
+                'filter' => 'number_int',
+                'value' => 99,
+            ),
+            'hide_main_fields' => array(
+                'title' => _a('Hide after complete main fields'),
+                'description' => '',
+                'edit' => 'checkbox',
+                'filter' => 'number_int',
+                'value' => 0,
+            ),
+        ),
+    ),
+);
