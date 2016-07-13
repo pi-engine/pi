812d04c32ddbdefec640ef2bb6dbda0d0fb5edeb
voltan <djvoltan@gmail.com>
2016-07-13 10:49:48 +0430

    Fix default theme

--- usr/theme/default/asset/js/admin.js
+++ usr/theme/default/asset/js/admin.js
@@ -1,16 +1,15 @@
-(function($) {
-  //For angularjs nav
-  var fragReg = /#!?(.*)$/;
-  $('#pi-nav-top').find('.nav-pills li').each(function() {
-    var $this = $(this);
-    var match = $this.find('a').attr('href').match(fragReg);
-    if (!match) return false;
-    $this.removeClass('active').attr('ng-class', 'navClass("' + match[1] + '")');
-  });
-  
-  //For debug
-  $(function() {
-    var debug = $('#pi-logger-output');
-    debug.length &&  debug.insertAfter($('.pi-module-content'));
-  });
+(function ($) {
+    //For angularjs nav
+    var fragReg = /#!?(.*)$/;
+    $('#pi-nav-top').find('.nav-pills li').each(function () {
+        var $this = $(this);
+        var match = $this.find('a').attr('href').match(fragReg);
+        if (!match) return false;
+        $this.removeClass('active').attr('ng-class', 'navClass("' + match[1] + '")');
+    });
+    //For debug
+    $(function () {
+        var debug = $('#pi-logger-output');
+        debug.length && debug.insertAfter($('.pi-module-content'));
+    });
 })(jQuery)
