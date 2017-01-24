832f7f719d415df4cca6e1426209926e09d1f0b7
Frédéric TISSOT <contact@espritdev.fr>
2017-01-24 18:07:12 +0100

    https://github.com/pi-engine/guide/issues/868 Optimize user bar - pass 3

--- lib/Pi/View/Helper/IsUserSection.php
+++ lib/Pi/View/Helper/IsUserSection.php
@@ -38,7 +38,7 @@ class IsUserSection extends AbstractHelper
         $uid = Pi::user()->getId();
         $userSection = false;
 
-        if (in_array($module, array('user', 'order', 'favourite', 'message')) && $uid > 0) {
+        if (in_array($module, array('user', 'order', 'favourite', 'message', 'support')) && $uid > 0) {
             $userSection = true;
         } elseif ($module == 'guide' && $uid > 0) {
             $d = (array) Pi::service('url')->getRouteMatch();
