dc27a7df7e16e70e1d23b6b4aea7b23875a32339
voltan <djvoltan@gmail.com>
2017-01-25 17:10:00 +0330

    Add user dashboard

---
+++ usr/module/user/src/Controller/Front/DashboardController.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt BSD 3-Clause License
+ */
+
+namespace Module\User\Controller\Front;
+
+use Pi;
+use Pi\Mvc\Controller\ActionController;
+
+/**
+ * Dashboard controller
+ *
+ * @author Hossein Azizabadi <azizabadi@faragostaresh.com>
+ */
+class DashboardController extends ActionController
+{
+    /**
+     * Edit base user information
+     *
+     * @return array|void
+     */
+    public function indexAction()
+    {
+        // Check login in
+        Pi::service('authentication')->requireLogin();
+        Pi::api('profile', 'user')->requireComplete();
+        $uid = Pi::user()->getId();
+
+        // Get identity, email, name
+        $user = Pi::api('user', 'user')->get(
+            $uid,
+            array('identity', 'email', 'name')
+        );
+        $user['uid']    = $uid;
+        $user['id']     = $uid;
+
+        $this->view()->assign(array(
+            'user'      => $user,
+        ));
+
+        $this->view()->headTitle(__('Account settings'));
+        $this->view()->headdescription(__('Basic settings'), 'set');
+        $this->view()->headkeywords($this->config('head_keywords'), 'set');
+    }
+}
