b8699787c9e8b3e4a393303fd78bc48181dbdad9
Frédéric TISSOT <contact@espritdev.fr>
2017-03-03 12:48:38 +0100

    https://github.com/pi-engine/guide/issues/732 Add media link (in progress)

--- lib/Pi/Db/RowGateway/RowGateway.php
+++ lib/Pi/Db/RowGateway/RowGateway.php
@@ -9,11 +9,11 @@
 
 namespace Pi\Db\RowGateway;
 
+use Pi;
 use Pi\Db\Table\AbstractTableGateway;
 use Zend\Db\RowGateway\RowGateway as AbstractRowGateway;
 use Zend\Db\Adapter\Adapter;
 use Zend\Db\Sql\Sql;
-use Zend\Db\RowGateway\Feature;
 use Zend\Db\RowGateway\Exception;
 
 /**
@@ -93,6 +93,14 @@ class RowGateway extends AbstractRowGateway
         $this->model = $model;
 
         return $this;
+    }
+
+    /**
+     * @return AbstractTableGateway
+     */
+    public function getModel()
+    {
+        return $this->model;
     }
 
     /**
@@ -362,6 +370,11 @@ class RowGateway extends AbstractRowGateway
 
             // make sure data and original data are in sync after save
             $this->populate($rowData, true);
+        }
+
+        $model = $this->getModel();
+        if(Pi::service('module')->isActive('media') && $model instanceof \Pi\Application\Model\Model && $model->getMediaLinks()){
+            Pi::api('link', 'media')->updateLinks($this);
         }
 
         // return rows affected
