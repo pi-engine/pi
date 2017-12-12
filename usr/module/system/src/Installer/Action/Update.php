c07ea2bd9f3337902a335a62f84d4faa6129c401
Mickaël STAMM <contact@sta2m.com>
2017-12-12 12:44:34 +0100

    https://github.com/pi-engine/guide/issues/1307

--- usr/module/system/src/Installer/Action/Update.php
+++ usr/module/system/src/Installer/Action/Update.php
@@ -71,7 +71,7 @@ class Update extends BasicUpdate
     public function updateSchema(Event $e)
     {
         $moduleVersion = $e->getParam('version');
-        $updator = new Schema\Updator3513($this);
+        $updator = new Schema\Updator361($this);
         $result = $updator->upgrade($moduleVersion);
 
         return $result;
