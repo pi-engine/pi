8ba9a9239f594c896a0c45b390df374886fd9da1
Frédéric TISSOT <contact@espritdev.fr>
2018-02-05 15:05:11 +0100

    https://github.com/pi-engine/guide/issues/1146

---
+++ usr/module/user/src/Controller/Front/CronController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
+ */
+
+/**
+ * @author Frédéric TISSOT
+ */
+namespace Module\User\Controller\Front;
+
+use Pi;
+use Pi\Mvc\Controller\ActionController;
+
+class CronController extends ActionController
+{
+    public function cleanOldSessionAction()
+    {
+        Pi::api('cron', 'user')->cleanOldSession();
+
+        $this->response->setStatusCode(200);
+        return array(
+            'message' => "Clean session Ok",
+        );
+    }
+}
