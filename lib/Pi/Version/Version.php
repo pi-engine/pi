13695895dba4545c2f439ffd30a17d2fc554e75d
voltan <djvoltan@gmail.com>
2018-03-20 10:40:15 +0330

    Update pi version to 2.6.0 stable

--- lib/Pi/Version/Version.php
+++ lib/Pi/Version/Version.php
@@ -27,7 +27,7 @@ class Version
      * @var string
      * @see http://semver.org/ for semantic versioning
      */
-    const VERSION = '2.6.0-rc1';
+    const VERSION = '2.6.0';
 
     /**
      * The latest stable version Pi Engine available
