298b0d9a2bf04d0611e89e13f1722347d22e56c9
Mickaël STAMM <contact@sta2m.com>
2018-06-19 13:19:38 +0100

    https://github.com/pi-engine/guide/issues/1436

---
+++ lib/Pi/Form/View/Helper/FormHtmlBrut.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Pi Engine (http://piengine.org)
+ *
+ * @link            http://code.piengine.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://piengine.org
+ * @license         http://piengine.org/license.txt BSD 3-Clause License
+ * @package         Form
+ */
+
+namespace Pi\Form\View\Helper;
+
+use Zend\Form\ElementInterface;
+
+/**
+ * HTML render helper
+ *
+ * @author Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
+ */
+class FormHtmlBrut extends AbstractHelper
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function render(ElementInterface $element, $options = [])
+    {
+        return $element->getValue();
+    }
+}
