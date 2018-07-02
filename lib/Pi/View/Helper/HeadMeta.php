0bd8963affe1e6b6580b17bab28a4179cf57462d
Frédéric TISSOT <contact@espritdev.fr>
2018-07-02 10:19:44 +0200

    https://github.com/pi-engine/guide/issues/1377 Fix vars

--- lib/Pi/View/Helper/HeadMeta.php
+++ lib/Pi/View/Helper/HeadMeta.php
@@ -60,7 +60,7 @@ class HeadMeta extends ZendHeadMeta
                 return '';
             }
         }
-        if ($item->property == 'og:title' || $item->name == 'twitter:title') {
+        if ((!empty($item->property) && $item->property == 'og:title') || (!empty($item->name) && $item->name == 'twitter:title')) {
             $this->_disableEscape = true;
         } else {
             $this->_disableEscape = false;
