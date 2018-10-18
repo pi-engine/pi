5075fce905aa42a9860cb5dab33597f00adafd50
Frédéric TISSOT <contact@espritdev.fr>
2018-10-18 16:26:35 +0200

    Update gitignore for modules

--- Gruntfile-config.js
+++ Gruntfile-config.js
@@ -3,10 +3,10 @@ module.exports = {
    * Which modules will be handler by grunt
    * Array list or string 'all'
    */
-   modules: ['system', 'user'],
+   modules: 'all',
   /* 
    * Which themes will be handler by grunt
    * Array list or string 'all'
    */
-  themes: ['enviedequeyras']
+  themes: 'all'
 }
