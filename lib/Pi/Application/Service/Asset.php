55efe7c5b6ddd373328aa603c699db897638659f
Frédéric TISSOT <contact@espritdev.fr>
2018-10-31 16:13:37 +0100

    Disable theme inheritance for admin, as asset publish processing would generate incorrect assets

--- lib/Pi/Application/Service/Asset.php
+++ lib/Pi/Application/Service/Asset.php
@@ -310,28 +310,30 @@ class Asset extends AbstractService
     {
         $theme     = $theme ?: Pi::service('theme')->current();
 
-        /**
-         * Check if theme has parent
-         */
-        $parentTheme = Pi::service('theme')->getParent($theme);
+        if(Pi::engine()->section() != 'admin'){
+            /**
+             * Check if theme has parent
+             */
+            $parentTheme = Pi::service('theme')->getParent($theme);
 
-        if($parentTheme){
+            if($parentTheme){
 
-            $themeAssetPath = $this->getThemeAssetPath($file, $theme, $appendVersion);
+                $themeAssetPath = $this->getThemeAssetPath($file, $theme, $appendVersion);
 
-            if(!is_file($themeAssetPath)){
-                $theme = $parentTheme;
+                if(!is_file($themeAssetPath)){
+                    $theme = $parentTheme;
 
-                /*
-                * Check if theme has grand parent
-                */
-                $parentTheme = Pi::service('theme')->getParent($theme);
+                    /*
+                    * Check if theme has grand parent
+                    */
+                    $parentTheme = Pi::service('theme')->getParent($theme);
 
-                if($parentTheme){
-                    $themeAssetPath = $this->getThemeAssetPath($file, $theme, $appendVersion);
+                    if($parentTheme){
+                        $themeAssetPath = $this->getThemeAssetPath($file, $theme, $appendVersion);
 
-                    if(!is_file($themeAssetPath)){
-                        $theme = $parentTheme;
+                        if(!is_file($themeAssetPath)){
+                            $theme = $parentTheme;
+                        }
                     }
                 }
             }
