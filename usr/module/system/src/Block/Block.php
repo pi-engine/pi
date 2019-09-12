2e6948e7ec3f14fcc532558b8350e54d923d2bed
Frédéric TISSOT <contact@espritdev.fr>
2019-09-12 15:42:48 +0200

    Queyraft BS4 + some fixes

--- usr/module/system/src/Block/Block.php
+++ usr/module/system/src/Block/Block.php
@@ -158,7 +158,7 @@ class Block
                 $defaultController = 'dashboard';
 
 
-                if (Pi::service('module')->isActive('user')) {
+                if (Pi::service('module')->isActive('order')) {
                     $uid = Pi::service('user')->getId();
 
                     $orderModel = Pi::model('order', 'order');
