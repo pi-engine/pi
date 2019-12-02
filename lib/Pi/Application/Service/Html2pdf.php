3e120d9502831666f48633324da1719d1a8be3d5
Hossein Azizabadi Farahani <djvoltan@gmail.com>
2019-12-02 09:14:48 +0330

    Improve and fix pdf

--- lib/Pi/Application/Service/Html2pdf.php
+++ lib/Pi/Application/Service/Html2pdf.php
@@ -10,6 +10,8 @@
 namespace Pi\Application\Service;
 
 use Pi;
+use Pi\View\Resolver\ModuleTemplate;
+use Spipu\Html2Pdf\Html2Pdf as SpipuHtml2Pdf;
 
 /**
  * @author Mickaël STAMM <contact@sta2m.com>
@@ -18,17 +20,19 @@ class Html2pdf extends AbstractService
 {
     public function pdf($template, $data, $name = 'document.pdf')
     {
-        require_once Pi::path('vendor') . '/autoload.php';
+        ob_end_clean();
+        //require_once Pi::path('vendor') . '/autoload.php';
 
-        $moduletemplate = new \Pi\View\Resolver\ModuleTemplate();
-        $template       = $moduletemplate->resolve($template, Pi::engine()->application()
-            ->getServiceManager()->get('view_manager')->getRenderer(), strstr($template, 'front'));
+        $moduleTemplate = new ModuleTemplate();
+        $template       = $moduleTemplate->resolve(
+            $template, Pi::engine()->application()->getServiceManager()->get('view_manager')->getRenderer(), strstr($template, 'front')
+        );
 
         // Get HTML
         $html = Pi::service('view')->render($template, $data);
 
         // Generate PDF
-        $html2pdf = new \Spipu\Html2Pdf\Html2Pdf();
+        $html2pdf = new SpipuHtml2Pdf();
         $html2pdf->writeHTML($html);
         $html2pdf->pdf->SetJPEGQuality(90);
         $html2pdf->pdf->setImageScale(1.53);
