7299a661206adad1ab76977cae878b8984badde1
voltan <djvoltan@gmail.com>
2020-03-16 15:10:23 +0330

    Update submit color

--- lib/Pi/Form/Element/Submit.php
+++ lib/Pi/Form/Element/Submit.php
@@ -26,7 +26,7 @@ class Submit extends Element
     protected $attributes
         = [
             'type'  => 'submit',
-            'class' => 'btn btn-secondary',
+            'class' => 'btn btn-primary',
         ];
 
     /**
